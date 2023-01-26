8d131b142a251a47990f925bbbda7ec3962fb2df
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-01-25 21:59:08 -0800

    Short URL will redirect to the appropriate long URL

--- express_server.js
+++ express_server.js
@@ -40,6 +40,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars)
 })
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[`${req.params.id}`];
+  res.redirect(302, `${longURL}`);
+})
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
