4acb6404bf4b203debdb0bd8b9ca6e8682d57d71
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-01-25 21:29:25 -0800

    After posting new url form will redirect to /urls/:id

--- express_server.js
+++ express_server.js
@@ -28,14 +28,16 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(302, `/urls/${shortURL}`);
+})
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars)
-})
-
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' 
 })
 
 app.get("/urls.json", (req, res) => {
