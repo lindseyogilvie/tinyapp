0b33b2de44db6448b2e0ea6797dd7ef27cdd7469
Lindsey Ogilvie <linds.ogilvie13@hotmail.com>
2023-01-26 22:07:36 -0800

    Modified registration endpoint to return errors if email or password inputs are empty or if email is already in use

--- express_server.js
+++ express_server.js
@@ -13,10 +13,21 @@ app.use(cookieParser());
 // Function generates random string consisting of 6 alphanumeric characters
 const generateRandomString = function() {
   return Math.random().toString(36).slice(2, 8);
-}
+};
 
 // Users database object
-const users = {};
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -34,19 +45,30 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
+  // If either the email or password input is empty, return error 400
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send("Please provide an email and a password.");
+  }
+  // If someone tries to register with an existing email address, return error 400
+  for (let userID in users) {
+    if (users[userID]["email"] === req.body.email) {
+      return res.status(400).send("Email is already in use.");
+    }
+  }
+
   const userID = generateRandomString();
   users[userID] = {};
   users[userID]["id"] = userID;
   users[userID]["email"] = req.body.email;
   users[userID]["password"] = req.body.password;
-  res.cookie('user_id', userID); 
-  res.redirect("/urls")
+  res.cookie('user_id', userID);
+  res.redirect("/urls");
 });
 
 // Get /urls -> My URLs page
 app.get("/urls", (req, res) => {
   const user = users[req.cookies["user_id"]];
-  const templateVars = { 
+  const templateVars = {
     urls: urlDatabase,
     user,
   };
@@ -55,25 +77,25 @@ app.get("/urls", (req, res) => {
 
 // Create cookie to save login username
 app.post("/login", (req, res) => {
-  const username = req.body.username
-  res.cookie('username', username); 
+  const username = req.body.username;
+  res.cookie('username', username);
   res.redirect("/urls");
-})
+});
 
 // Logout and clear username cookie
 app.post("/logout", (req, res) => {
   res.clearCookie('username');
   res.redirect("/urls");
-})
+});
 
 // Get /urls/new -> New Short URL page
 app.get("/urls/new", (req, res) => {
   const user = users[req.cookies["user_id"]];
   const templateVars = {
     user,
-    };
+  };
   res.render("urls_new", templateVars);
-})
+});
 
 // Generate short URL and redirect to short URL page
 app.post("/urls", (req, res) => {
@@ -81,37 +103,37 @@ app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(302, `/urls/${shortURL}`);
-})
+});
 
 // Get /urls/:id -> short URL page
 app.get("/urls/:id", (req, res) => {
   const user = users[req.cookies["user_id"]];
-  const templateVars = { 
-    id: req.params.id, 
-    longURL: urlDatabase[req.params.id], 
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
     user,
   };
-  res.render("urls_show", templateVars)
-})
+  res.render("urls_show", templateVars);
+});
 
 // Use short URL to redirect to corresponding long URL webpage
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[`${req.params.id}`];
   res.redirect(longURL);
-})
+});
 
 // Update short URL to correspond to a different long URL
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
-  urlDatabase[id] = req.body.longURL; 
-  res.redirect("/urls") 
-})
+  urlDatabase[id] = req.body.longURL;
+  res.redirect("/urls");
+});
 
 // Delete short URL
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id]
+  delete urlDatabase[req.params.id];
   res.redirect("/urls");
-})
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
